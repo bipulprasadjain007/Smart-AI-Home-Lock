f57ea1aa7eacb087d54847594311b2423c6df2a1
Bipul Prasad <bipulprasad14.1.2003@gmail.com>
2026-07-03 10:41:54 +0530

    chore: remove test images from repo (privacy sensitive)

--- test-images/images-encryption.py
+++
@@ -1,9 +0,0 @@
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-key = bytes.fromhex('2b7e151628aed2a6abf7158809cf4f3c')
-cipher = AES.new(key, AES.MODE_ECB)
-with open('image5.jpeg', 'rb') as f:
-    data = f.read()
-encrypted = cipher.encrypt(pad(data, 16))
-with open('encrypted_image5.jpg', 'wb') as f:
-    f.write(encrypted)
